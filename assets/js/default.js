201a1ab99e81837a1f4e2d2ce4f877b84f79daa9
gustavoarruda <gustavosantosdev@gmail.com>
2014-05-25 12:47:07 -0300

    Carrinho

--- assets/js/default.js
+++ assets/js/default.js
@@ -26,7 +26,7 @@ $(function() {
 			soloArrowRightValign:"center",
 			soloArrowRightHOffset:20,
 			soloArrowRightVOffset:0,
-			touchenabled:"on",
+			touchenabled:"off",
 			onHoverStop:"on",
 			stopAtSlide:-1,
 			stopAfterLoops:-1,
@@ -38,9 +38,60 @@ $(function() {
 		});
 	}
 
-	
+});
 
-	
+	$(document).ready(function(){
+	$('#button-area-aluno').click(function() {
+		$(this).next('#form-aluno').slideToggle();
+		$(this).toggleClass('active');
+	})
+
+	$('#carrinho-label').click(function() {
+		$(this).next('#itens-do-carrinho').slideToggle();
+		$(this).toggleClass('active');
+	})
+
 
 
 });
+
+	jQuery(function($) {
+	$('ul#items').easyPaginate({
+		step : 3,
+		auto : false,
+		loop : false,
+		clickstop : false,
+		pause : 8000
+	});
+
+$("#createWindowDraggable").on('click', function(){
+                            $.Dialog({
+                                shadow: true,
+                                overlay: false,
+                                draggable: true,
+                                icon: '<span class="icon-bus"></span>',
+                                title: 'Draggable window',
+                                width: 500,
+                                padding: 10,
+                                content: 'This Window is draggable by caption.',
+                                onShow: function(){
+                                    var content = '<form id="login-form-1">' +
+                                            '<label>Login</label>' +
+                                            '<div class="input-control text"><input type="text" name="login"><button class="btn-clear"></button></div>' +
+                                            '<label>Password</label>'+
+                                            '<div class="input-control password"><input type="password" name="password"><button class="btn-reveal"></button></div>' +
+                                            '<div class="input-control checkbox"><label><input type="checkbox" name="c1" checked/><span class="check"></span>Check me out</label></div>'+
+                                            '<div class="form-actions">' +
+                                            '<button class="button primary">Login to...</button>&nbsp;'+
+                                            '<button class="button" type="button" onclick="$.Dialog.close()">Cancel</button> '+
+                                            '</div>'+
+                                            '</form>';
+
+                                    $.Dialog.title("User login");
+                                    $.Dialog.content(content);
+                                }
+
+                            });
+                        });
+});
+
